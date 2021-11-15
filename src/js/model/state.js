99099941d4e0102b10bf07e667b68edbb1cf3331
blacktoast <dbstjd100@naver.com>
2021-11-15 22:34:29 +0900

    feat: 입력금액 상태 저장 함수 구현

--- src/js/model/state.js
+++ src/js/model/state.js
@@ -1,6 +1,11 @@
 let state = {
+  inputPrice: 0,
   tickets: [],
 };
+
+export function setStateInputPrice(price) {
+  state.inputPrice = price;
+}
 
 export function setStateTicket(states) {
   state.tickets = states;
