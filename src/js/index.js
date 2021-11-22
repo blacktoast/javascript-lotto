9f75a7b6e94c3d967df1f09bbee2f294bc386d9a
blacktoast <dbstjd100@naver.com>
2021-11-22 05:19:14 +0000

    file: 파일명 변경

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,7 @@
 import { handlerPurchaseLottoToggle } from "./handler/handlePurchaseResultToggle.js";
 import { checkLottoResult } from "./handler/handlerLottoResult.js";
 import { initModalEvents } from "./handler/modalEvent.js";
-import { purchaseLotto } from "./handler/purchaseLotto.js";
+import { purchaseLotto } from "./handler/handlerPurchaseLotto.js";
 const $lottoNumbersToggleButton = document.querySelector(
   ".lotto-numbers-toggle-button"
 );
